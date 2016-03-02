126325f367a37973d28392824c38fced2c6028c8
unknown <Ch0203@LT-CNU335BPM5.ad.laroverken.se>
2016-03-02 11:56:10 +0100

    Initial commit

---
+++ missing.js
@@ -0,0 +1,17 @@
+// var name = prompt("Vad heter du?"); var lastName = prompt("Vad heter du i efternamn?"); alert(name+" "+lastName);
+//function go(name, age){
+//  alert(name)
+//  alert(age)
+//}
+
+function add(first, second){
+  return first * second;
+}
+var sum = add(7, 3);
+// alert(sum);
+
+var myList = ["apples", "oranges", "bananas"]
+
+for (var i=0; i < myList.length; i++) {
+  //alert("you have " + myList[i])
+}
