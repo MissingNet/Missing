ac240ab65f2df310d23fb82b00b664bf146e1c5f
unknown <Ch0203@LT-CNU335BPM5.ad.laroverken.se>
2016-03-04 16:11:04 +0100

    Update 2

--- missing.js
+++ missing.js
@@ -1,13 +1,12 @@
-$(document).ready(function(){
-  $("#newLost").submit(function(e) {
-    e.preventDefault();
-    saveLost();
+var Item = parse.Object.extend("Item");
+var query = new Parse.Query(Item);
+query.equalTo("Lost", true);
+query.find({
+  success:funtion(results) {
+
+    }
+
+
+    
   })
-})
-
-function saveLost() {
-  var Item = parse.Object.extend("Item");
-  
-
-
 }
