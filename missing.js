7fb9f9cf3b530a59a3b44682ec99df8cdba42470
unknown <Ch0203@LT-CNU335BPM5.ad.laroverken.se>
2016-03-06 17:42:53 +0100

    Uppdaterat JavaScrip. Nu kan man lägga till annonser. Allt hanmnar på en sidan

--- missing.js
+++ missing.js
@@ -1,12 +1,50 @@
+
 var Item = Parse.Object.extend("Item");
-var query = new Parse.Query(Item);
-query.equalTo("Lost", true);
-query.find({
-  success:funtion(results) {
+
+function getItems() {
+  var query = new Parse.Query(Item);
+  query.find({
+    success: function(results){
+        var output = "";
+        for (var i in results) {
+          var title = results[i].get("title");
+          var description = results[i].get("description");
+          var name = results[i].get("name");
+
+
+          output += "<li>";
+          output += "<h3>"+title+"</h3>";
+          output += "<p>"+description+"</p>";
+          output += "<p>"+"Hittat av:"+name+"</p>";
+
+          output += "</li>";
+        }
+        $("#list-items").html(output);
+    }, error: function(){
+      console.log("Query Error"+error.message);
 
     }
-
-
-
-  })
+  });
 }
+
+getItems();
+
+$("#post-form").submit(function(event){
+  event.preventDefault();
+    var title = $("#post-title").val();
+    var description = $("#post-description").val();
+    var name = $("#post-name").val();
+
+    var newItem = new Item();
+    newItem.set("title", title);
+    newItem.set("description",description);
+    newItem.set("name", name);
+
+    newItem.save({
+      success: function(){
+        getItems();
+      }, error: function(error){
+        console.log("Error:"+error.message);
+      }
+    });
+  });
